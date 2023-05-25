91db129083f1956d2d69742524a4fed3d177569f
Tihanyi Marcell <marcitihanyi@gmail.com>
2023-05-25 10:36:16 +0200

    SQL Structure

--- SQL_Structure.sql
+++ SQL_Structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2023 at 09:47 PM
+-- Generation Time: May 25, 2023 at 10:35 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
