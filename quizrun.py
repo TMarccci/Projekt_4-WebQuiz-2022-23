9f92a2de6dbebbf7f3ddad8b99f316e270844d56
Tihanyi Marcell <marcitihanyi@gmail.com>
2023-05-29 22:55:14 +0200

    Reloader

--- quizrun.py
+++ quizrun.py
@@ -1,4 +1,4 @@
 from werkzeug.serving import run_simple
 from app import *
 
-run_simple('localhost', 5004, app, use_reloader=True)
+run_simple('localhost', 5004, app, use_reloader=True, threaded=True)
